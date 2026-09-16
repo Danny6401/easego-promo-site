cc750a1b5403719b8213df8744256eafc52e9e71
Danny <danny602@gmail.com>
2026-09-16 15:36:42 +0800

    feat:新增九份喜來園景觀民宿

--- src/data/site-content.js
+++ src/data/site-content.js
@@ -36,7 +36,7 @@ export const homeContent = {
     islands: "islands",
     overseas: "overseas",
   },
-  featuredPartnerSlugs: ["peach-holiday-manoranor", "jiufen-light-travel", "siban-bnb", "brookview-ecological-fram", "flower-duke-duck"],
+  featuredPartnerSlugs: ["peach-holiday-manoranor", "jiufen-light-travel", "siban-bnb", "brookview-ecological-fram", "flower-duke-duck", "happyland-bnb"],
   caseItems: [
     {
       slug: "peach-holiday-manoranor",
